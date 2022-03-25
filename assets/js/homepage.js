eacc09a8901cae45d8700e29e3793211297cf991
JamieSteinbauer <jsteinbauer14@gmail.com>
2022-03-25 00:18:24 -0400

    list issues and error handling

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -34,11 +34,13 @@ var formSubmitHandler = function(event) {
 
   if (username) {
     getUserRepos(username);
+
+    //clear old content
+    repoContainerEl.textContent = "";
     nameInputEl.value = "";
   } else {
     alert("Please enter a GitHub username");
   }
-  console.log(event);
 };
 
 userFormEl.addEventListener("submit", formSubmitHandler);
@@ -58,8 +60,9 @@ var displayRepos = function(repos, searchTerm) {
     var repoName = repos[i].owner.login + "/" + repos[i].name;
 
     //create a container for each repo
-    var repoEl = document.createElement("div");
+    var repoEl = document.createElement("a");
     repoEl.classList = "list-item flex-row justify-space-between align-center";
+    repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
 
     //create a span element to hold repository name
     var titleEl = document.createElement("span");
